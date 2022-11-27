5e7e17226f9509295c3147796b8c6b556b64a6ca
aEnchev669 <aenchev669@abv.bg>
2022-11-27 13:08:05 +0200

    Interface

---
+++ 2. Cars/2. Cars/Tesla.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cars
+{
+
+    public class Tesla : ICar, IElectricCar
+    {
+        public Tesla(string model, string color, int battery)
+        {
+            Model = model;
+            Color = color;
+            Battery = battery;
+        }
+
+        public string Model { get; set; }
+        public string Color { get; set; }
+        public int Battery { get ; set; }
+        public string Start { get; set; }
+        public string Stop { get; set; }
+
+        public override string ToString()
+        {
+            return $"{this.Color} {nameof(Tesla)} {this.Model} with {Battery} Batteries";
+            
+        }
+    }
+}
