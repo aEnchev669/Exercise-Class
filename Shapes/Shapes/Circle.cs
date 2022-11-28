6fe1cacd714d2dc132a282906e679c9c967e6242
aEnchev669 <aenchev669@abv.bg>
2022-11-28 22:25:52 +0200

    Polymorphism

---
+++ Shapes/Shapes/Circle.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shapes
+{
+    public class Circle : Shape
+    {
+        private double radius;
+        public Circle(double radius)
+        {
+            this.radius = radius;
+        }
+
+        public override double CalculateArea() => Math.PI * Math.Pow(radius, 2);
+
+        public override double CalculatePerimeter() => Math.PI * radius * 2;
+
+        public override string Draw()
+        {
+            return base.Draw() + GetType().Name;
+        }
+    }
+}
